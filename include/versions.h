ff22ca56be54d02288824ba851e419933be34dea
muletto42 <muletto42@web.de>
2025-08-11 21:05:22 +0200

    Es wird!

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAIN_Version "925974b"
+#define MAIN_Version "2468816"
 #define KNX_Version "2.2.0+4fb43ed"
 #define MODULE_FileTransferModule_Version "0.1.1+75e4c00"
 #define MODULE_FileTransferModule_Version_Major 0
@@ -12,4 +12,9 @@
 #define MODULE_Common_Version_Minor 4
 #define MODULE_Common_Version_Revision 3
 #define MODULE_Common_ETS 20
-#define MODULE_PowerAmp_Version "28426cc"
+#define MODULE_LogicModule_Version "3.7.1+1ba35cb"
+#define MODULE_LogicModule_Version_Major 3
+#define MODULE_LogicModule_Version_Minor 7
+#define MODULE_LogicModule_Version_Revision 1
+#define MODULE_LogicModule_ETS 55
+#define MODULE_PowerAmp_Version "220e759"
